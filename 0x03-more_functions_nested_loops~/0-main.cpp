a6249884fc2a58ee1a731ef6fe43e04cb1123bfd
Mennaasamir1 <monyy_20174@outlook.com>
2024-10-20 12:41:58 +0300

    new task

---
+++ 0x03-more_functions_nested_loops~/0-main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 or 1
+ */
+int main(void)
+{
+    int character = 95;
+
+    cout << "is " << character << " in uppercase? " << _isupper(character) << endl;
+
+    return (0);
+}

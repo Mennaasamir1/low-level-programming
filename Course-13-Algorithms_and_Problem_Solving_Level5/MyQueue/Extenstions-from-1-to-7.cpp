cd67334c949ca10af785def7950d36bbea4aa760
Mennaasamir1 <monyy_20174@outlook.com>
2025-10-11 12:15:07 +0300

    Extensions from 1 to 7

--- Course-13-Algorithms_and_Problem_Solving_Level5/MyQueue/Extenstions-from-1-to-7.cpp
+++ Course-13-Algorithms_and_Problem_Solving_Level5/MyQueue/Extenstions-from-1-to-7.cpp
@@ -22,6 +22,39 @@ int main(void)
     cout << "\nQueue after pop(): ";
     MyQueue.Print();
 
+    /* Extension 1 */
+
+    cout << "\nItem(2): " << MyQueue.GetItem(2) << endl;
+
+    /* Extension 2 */
+    MyQueue.Reverse();
+    cout << "\nQueue after reverse: ";
+    MyQueue.Print();
+
+    /* Extension 3 */
+    MyQueue.UpdateItem(2, 600);
+    cout << "\nQueue after updating Item(2) to 600 : \n";
+    MyQueue.Print();
+
+    /* Extension 4 */
+    MyQueue.InsertAfter(2, 800);
+    cout << "\nQueue after Inserting 800 after Item(2) : \n";
+    MyQueue.Print();
+
+    /* Extension 5 */
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nQueue after Inserting 1000 at front: \n";
+    MyQueue.Print();
+
+    /* Extension 6 */
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nQueue after Inserting 2000 at back: \n";
+    MyQueue.Print();
+
+    /* Extension 7 */
+    MyQueue.Clear();
+    cout << "\n\nQueue after Clear(): \n";
+    MyQueue.Print();
     
     return (0);
 }
